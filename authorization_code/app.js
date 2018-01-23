b2c21db0643198c06fbd3c0674f0bf59c7b25ae3
Pontus Alexander <pontus.alexander@gmail.com>
2018-01-23 18:21:13 +0100

    Do not serve public.

--- authorization_code/app.js
+++ authorization_code/app.js
@@ -36,8 +36,7 @@ var stateKey = 'spotify_auth_state';
 
 var app = express();
 
-app.use(express.static(__dirname + '/public'))
-   .use(cookieParser());
+app.use(cookieParser());
 
 app.get('/login', function(req, res) {
   var state = generateRandomString(16);
